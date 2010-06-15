553a19a6dbc26cbeaf42e1513401916ecea4a6de
sinasheikh.13 <sinasheikh.13@gmail.com>
2010-06-15 22:44:09 +0000

    ATS revision 3

--- ATS/src/ats/classifiers/BayesNetwork.java
+++ ATS/src/ats/classifiers/BayesNetwork.java
@@ -7,18 +7,17 @@ import java.util.Random;
 
 import javax.swing.JFrame;
 
-import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.BayesianLogisticRegression;
-import weka.classifiers.trees.J48;
-import weka.core.Instances;
-import weka.gui.graphvisualizer.GraphVisualizer;
-import weka.gui.treevisualizer.PlaceNode2;
-import weka.gui.treevisualizer.TreeVisualizer;
 import ats.DataSet;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.BayesNet;
+import weka.core.Instances;
+import weka.gui.graphvisualizer.BIFFormatException;
+import weka.gui.graphvisualizer.GraphVisualizer;
 
 public class BayesNetwork {
 
-	private BayesianLogisticRegression bayesNet = null;
+	private BayesNet bayesNet = null;
+	ArrayList<double[]> classificationResultSet = new ArrayList<double[]>();
 
 	public BayesNetwork() {
 		super();
@@ -28,8 +27,8 @@ public class BayesNetwork {
 	public void buildBayesNet(Instances trainingData) {
 
 		try {
-			bayesNet = new BayesianLogisticRegression(); // new instance of
-															// bayesian network
+			bayesNet = new BayesNet();
+			// Train classifier
 			bayesNet.buildClassifier(trainingData);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -40,14 +39,13 @@ public class BayesNetwork {
 	// Classify test data set
 	public void classify(Instances testData) {
 
-		ArrayList<Double> classificationResultSet = new ArrayList<Double>();
-		Double classificationResult = null;
+		double[] classificationResult = null;
 
 		for (int i = 0; i < testData.numInstances(); i++) {
 
 			try {
-				classificationResult = bayesNet.classifyInstance(testData
-						.get(i));
+				classificationResult = bayesNet
+						.distributionForInstance(testData.get(i));
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -55,24 +53,19 @@ public class BayesNetwork {
 
 			classificationResultSet.add(i, classificationResult);
 		}
-
-		for (int i = 0; i < classificationResultSet.size(); i++) {
-			System.out.println(classificationResultSet.get(i));
-		}
 	}
 
 	// Evaluate classifier through cross validation
 	public void crossValidate() {
 
 		Instances newData = new Instances(new DataSet().getInstances());
-
 		// Remove closing price "close" attribute
 		newData.deleteAttributeAt(0);
 
 		try {
 			Evaluation eval = new Evaluation(newData);
-			BayesianLogisticRegression untrainedDecitionTree = new BayesianLogisticRegression();
-			eval.crossValidateModel(untrainedDecitionTree, newData, newData
+			BayesNet untrainedBayesNet = new BayesNet();
+			eval.crossValidateModel(untrainedBayesNet, newData, newData
 					.numInstances(), new Random(1));
 			System.out.println(eval.toSummaryString("\nResults\n\n", false));
 		} catch (Exception e) {
@@ -81,26 +74,38 @@ public class BayesNetwork {
 		}
 	}
 
-	
-	  public void getInternalModel(Instances trainingData) {
-	  
-		/*  
-		BayesNet cls = new BayesNet();
-		cls.buildClassifier(data);
-		// display graph
-		GraphVisualizer gv = new GraphVisualizer();
-		gv.readBIF(cls.graph());
-		JFrame jf = new JFrame("BayesNet graph");
-		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		jf.setSize(800, 600);
-		jf.getContentPane().setLayout(new BorderLayout());
-		jf.getContentPane().add(gv, BorderLayout.CENTER);
-		jf.setVisible(true);
-		// layout graph
-		gv.layoutGraph();
-		*/  
-	  
-	  }
-	 
+	public void getInternalModel(Instances trainingData) {
 
+		try {
+			// train classifier
+			BayesNet cls = new BayesNet();
+			trainingData.deleteAttributeAt(trainingData.numAttributes() - 2);
+			cls.buildClassifier(trainingData);
+			// display graph
+			GraphVisualizer gv = new GraphVisualizer();
+			gv.readBIF(cls.graph());
+			JFrame jf = new JFrame("BayesNet graph");
+			jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			jf.setSize(800, 600);
+			jf.getContentPane().setLayout(new BorderLayout());
+			jf.getContentPane().add(gv, BorderLayout.CENTER);
+			jf.setVisible(true);
+			// layout graph
+			gv.layoutGraph();
+		} catch (HeadlessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BIFFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// Get classification result
+	public ArrayList<double[]> getClassificationResultSet() {
+		return classificationResultSet;
+	}
 }
